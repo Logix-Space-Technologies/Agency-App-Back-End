0a64e53a8245b16555ff8d04b951b369a5b83a60
Aseera092 <aseeraachu895@gmail.com>
2025-03-24 21:04:27 +0530

    category delete completed

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getCategories, addCategory } = require('../controllers/categoryController');
+const { getCategories, addCategory, deleteCategory } = require('../controllers/categoryController');
 
 router.post('/', getCategories);
 router.post('/add', addCategory);
+router.post('/delete', deleteCategory);
 
 module.exports = router;
