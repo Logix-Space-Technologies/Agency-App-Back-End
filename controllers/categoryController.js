43f5de687ad615627cd3d03127d90c68664a9acd
kavyamnaier123 <kavyamnair7@gmail.com>
2025-04-06 00:09:23 +0530

    search completed

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -36,5 +36,19 @@ exports.deleteCategory = async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Database error' });
     }
-}
+};
+
+
+// Add Serach Category
+exports.searchCategory = async (req, res) => {
+    try {
+        const { category_name } = req.body;
+        if (!category_name) return res.status(400).json({ error: "Category name is required" });
+
+        const [result] = await pool.query('SELECT `category_id`, `category_name` FROM `categories` WHERE `category_name`=?', [category_name]);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Database error' });
+    }
+};
 
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getCategories, addCategory, deleteCategory } = require('../controllers/categoryController');
+const { getCategories, addCategory, deleteCategory, searchCategory } = require('../controllers/categoryController');
 
 router.post('/', getCategories);
 router.post('/add', addCategory);
 router.post('/delete', deleteCategory);
+router.post('/search', searchCategory);
 
 module.exports = router;
