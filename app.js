fd0924d095a029935dc1b999a225e0a01ee2d727
ARJ-U-N <ghub70242@gmail.com>
2025-04-12 21:08:11 +0530

    suppliers completed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require("./routes/productRouts");
 const productPriceRoutes = require("./routes/productPriceRoures");
 const userRoutes = require("./routes/userRoutes");
+const supplierRoutes =require("./routes/supplierRoutes");
 
 const app = express();
 app.use(cors());
@@ -18,6 +19,7 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/products',productRoutes);
 app.use('/api/productprice',productPriceRoutes)
 app.use('/api/users',userRoutes)
+app.use('/api/suppliers',supplierRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
