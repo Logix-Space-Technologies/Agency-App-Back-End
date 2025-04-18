d9aa556d66c346d8ddcedd1bc1a4fa424318ef98
anishpdm <anish.vilayil.s@gmail.com>
2025-04-18 23:02:24 +0530

    New feature Purchases

--- AgencyDb.sql
+++ AgencyDb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 24, 2025 at 10:58 AM
+-- Generation Time: Apr 18, 2025 at 11:00 PM IST
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -29,15 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `brands` (
   `brand_id` int(11) NOT NULL,
-  `brand_name` varchar(255) NOT NULL
+  `brand_name` varchar(255) NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `brands`
---
-
-INSERT INTO `brands` (`brand_id`, `brand_name`) VALUES
-(7, ' Milma');
 
 -- --------------------------------------------------------
 
@@ -47,7 +41,8 @@ INSERT INTO `brands` (`brand_id`, `brand_name`) VALUES
 
 CREATE TABLE `categories` (
   `category_id` int(11) NOT NULL,
-  `category_name` varchar(255) NOT NULL
+  `category_name` varchar(255) NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,7 +73,25 @@ CREATE TABLE `daily_stock_allocation` (
   `marketing_staff_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `allocated_quantity` int(11) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1,
+  `converted_to_sales` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `final_sale`
+--
+
+CREATE TABLE `final_sale` (
+  `id` int(11) NOT NULL,
+  `sale_tracking_Id` varchar(555) NOT NULL,
+  `TotalAmount` double NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `DateofTransaction` timestamp NOT NULL DEFAULT current_timestamp(),
+  `isSettled` int(11) NOT NULL,
+  `AmountPaid` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -94,9 +107,10 @@ CREATE TABLE `products` (
   `brand_id` int(11) DEFAULT NULL,
   `mrp` decimal(10,2) NOT NULL,
   `description` text DEFAULT NULL,
-  `expiry_date` date DEFAULT NULL,
+  `expiry_date` int(11) DEFAULT NULL,
   `product_image` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -109,9 +123,12 @@ CREATE TABLE `product_prices` (
   `price_id` int(11) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
   `purchase_price` decimal(10,2) NOT NULL,
-  `marketing_selling_price` decimal(10,2) NOT NULL,
-  `direct_selling_price` decimal(10,2) NOT NULL,
-  `effective_date` date NOT NULL
+  `commision_rate` double NOT NULL DEFAULT 0,
+  `marketing_selling_price` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `direct_selling_price` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `whole_sale_price` double NOT NULL DEFAULT 0,
+  `effective_date` date NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -125,9 +142,17 @@ CREATE TABLE `purchase` (
   `product_id` int(11) NOT NULL,
   `purchase_date` date NOT NULL,
   `purchase_price` decimal(10,2) NOT NULL,
+  `total_amount` double DEFAULT NULL,
   `quantity` int(11) NOT NULL,
   `Invoice_Number` varchar(1100) NOT NULL,
-  `supplier_id` int(11) NOT NULL
+  `supplier_id` int(11) NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1,
+  `AddedDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_damaged` tinyint(1) DEFAULT 0,
+  `damage_description` text DEFAULT NULL,
+  `replacement_provided` tinyint(1) DEFAULT 0,
+  `replacement_date` date DEFAULT NULL,
+  `is_free_replacement` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -141,7 +166,22 @@ CREATE TABLE `returns` (
   `marketing_staff_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `damaged_quantity` int(11) NOT NULL,
-  `return_date` date NOT NULL
+  `return_date` date NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Salary`
+--
+
+CREATE TABLE `Salary` (
+  `id` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Amount` double NOT NULL,
+  `Sale_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -157,12 +197,14 @@ CREATE TABLE `sales` (
   `product_id` int(11) DEFAULT NULL,
   `price_id` int(11) DEFAULT NULL,
   `quantity_sold` int(11) NOT NULL,
-  `amount_received` decimal(10,2) NOT NULL,
+  `amount_received` double NOT NULL DEFAULT 0,
   `is_credit` int(11) DEFAULT 0,
+  `sale_tracking_Id` varchar(500) NOT NULL,
   `sale_date` date NOT NULL,
   `damaged_count` int(11) NOT NULL,
   `is_settled` int(11) NOT NULL,
-  `loss_count` int(11) NOT NULL
+  `loss_count` int(11) NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -173,9 +215,10 @@ CREATE TABLE `sales` (
 
 CREATE TABLE `sales_credit_history` (
   `sales_credit_history_id` int(11) NOT NULL,
-  `sale_id` int(11) NOT NULL,
+  `sale_tracking_Id` varchar(555) NOT NULL,
   `amount` int(11) NOT NULL,
-  `creditedDate` int(11) NOT NULL
+  `creditedDate` datetime NOT NULL DEFAULT current_timestamp(),
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -189,7 +232,27 @@ CREATE TABLE `stock` (
   `product_id` int(11) DEFAULT NULL,
   `price_id` int(11) DEFAULT NULL,
   `quantity` int(11) NOT NULL DEFAULT 0,
-  `added_date` timestamp NOT NULL DEFAULT current_timestamp()
+  `Damage_Qty` int(11) NOT NULL DEFAULT 0,
+  `Loss_Qty` int(11) NOT NULL DEFAULT 0,
+  `added_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `isActive` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock_History`
+--
+
+CREATE TABLE `stock_History` (
+  `stock_History_Id` int(11) NOT NULL,
+  `stock_Id` int(11) NOT NULL,
+  `Qty` int(11) NOT NULL,
+  `stock_type` varchar(555) DEFAULT NULL,
+  `AddedDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `AddedBy` int(11) NOT NULL DEFAULT 0,
+  `CreditOrDebit` varchar(1500) NOT NULL DEFAULT 'NULL',
+  `ReferenceInvoiceOrSale` varchar(1500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -204,7 +267,8 @@ CREATE TABLE `suppliers` (
   `added_Date` datetime NOT NULL DEFAULT current_timestamp(),
   `contact_number` varchar(2000) NOT NULL,
   `email_Id` varchar(2000) NOT NULL,
-  `Address` varchar(2000) NOT NULL
+  `Address` varchar(2000) NOT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -218,11 +282,12 @@ CREATE TABLE `users` (
   `profile_avathar` varchar(1100) NOT NULL,
   `name` varchar(255) NOT NULL,
   `role` enum('admin','staff','marketing_staff') NOT NULL,
-  `phone` varchar(15) NOT NULL,
+  `phone` varchar(15) NOT NULL DEFAULT '0',
   `email` varchar(255) NOT NULL,
   `password_hash` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `Place_Of_Allocation` varchar(5000) DEFAULT NULL
+  `Place_Of_Allocation` varchar(5000) DEFAULT NULL,
+  `isActive` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -260,6 +325,12 @@ ALTER TABLE `daily_stock_allocation`
   ADD KEY `product_id` (`product_id`);
 
 --
+-- Indexes for table `final_sale`
+--
+ALTER TABLE `final_sale`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -291,6 +362,12 @@ ALTER TABLE `returns`
   ADD KEY `product_id` (`product_id`);
 
 --
+-- Indexes for table `Salary`
+--
+ALTER TABLE `Salary`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sales`
 --
 ALTER TABLE `sales`
@@ -303,8 +380,7 @@ ALTER TABLE `sales`
 -- Indexes for table `sales_credit_history`
 --
 ALTER TABLE `sales_credit_history`
-  ADD PRIMARY KEY (`sales_credit_history_id`),
-  ADD KEY `fk_sales_credit` (`sale_id`);
+  ADD PRIMARY KEY (`sales_credit_history_id`);
 
 --
 -- Indexes for table `stock`
@@ -313,6 +389,12 @@ ALTER TABLE `stock`
   ADD PRIMARY KEY (`stock_id`),
   ADD KEY `product_id` (`product_id`),
   ADD KEY `price_id` (`price_id`);
+
+--
+-- Indexes for table `stock_History`
+--
+ALTER TABLE `stock_History`
+  ADD PRIMARY KEY (`stock_History_Id`);
 
 --
 -- Indexes for table `suppliers`
@@ -336,13 +418,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `credits`
@@ -354,129 +436,28 @@ ALTER TABLE `credits`
 -- AUTO_INCREMENT for table `daily_stock_allocation`
 --
 ALTER TABLE `daily_stock_allocation`
-  MODIFY `daily_stock_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `daily_stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
+
+--
+-- AUTO_INCREMENT for table `final_sale`
+--
+ALTER TABLE `final_sale`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `product_prices`
 --
 ALTER TABLE `product_prices`
-  MODIFY `price_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `price_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `returns`
---
-ALTER TABLE `returns`
-  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sales`
---
-ALTER TABLE `sales`
-  MODIFY `sale_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `sales_credit_history`
---
-ALTER TABLE `sales_credit_history`
-  MODIFY `sales_credit_history_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `stock`
---
-ALTER TABLE `stock`
-  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `suppliers`
---
-ALTER TABLE `suppliers`
-  MODIFY `supplier_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `credits`
---
-ALTER TABLE `credits`
-  ADD CONSTRAINT `credits_ibfk_1` FOREIGN KEY (`marketing_staff_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `credits_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `daily_stock_allocation`
---
-ALTER TABLE `daily_stock_allocation`
-  ADD CONSTRAINT `daily_stock_allocation_ibfk_1` FOREIGN KEY (`marketing_staff_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `daily_stock_allocation_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`) ON DELETE SET NULL,
-  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`brand_id`) ON DELETE SET NULL;
-
---
--- Constraints for table `product_prices`
---
-ALTER TABLE `product_prices`
-  ADD CONSTRAINT `product_prices_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `purchase`
---
-ALTER TABLE `purchase`
-  ADD CONSTRAINT `fk_purchase_supplier` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`supplier_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
-
---
--- Constraints for table `returns`
---
-ALTER TABLE `returns`
-  ADD CONSTRAINT `returns_ibfk_1` FOREIGN KEY (`marketing_staff_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `returns_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `sales`
---
-ALTER TABLE `sales`
-  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`marketing_staff_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL,
-  ADD CONSTRAINT `sales_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `sales_ibfk_3` FOREIGN KEY (`price_id`) REFERENCES `product_prices` (`price_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `sales_credit_history`
---
-ALTER TABLE `sales_credit_history`
-  ADD CONSTRAINT `fk_sales_credit` FOREIGN KEY (`sale_id`) REFERENCES `sales` (`sale_id`) ON DELETE CASCADE;
-
---
--- Constraints for table `stock`
---
-ALTER TABLE `stock`
-  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`price_id`) REFERENCES `product_prices` (`price_id`) ON DELETE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=
