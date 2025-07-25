3283f56b5989697b5b68c318a4fd89684b4bac4b
anishpdm <anish.vilayil.s@gmail.com>
2025-07-25 21:47:11 +0530

    chnages

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1369,7 +1369,12 @@ exports.fetchDirectSaleListByDate = async (req, res) => {
     const { date } = req.body;
     console.log(req.body);
     const [sales] = await pool.query(
-      "SELECT FS.id, FS.sale_tracking_Id, C.Name, FS.TotalAmount, FS.isSettled FROM `final_sale` as FS JOIN `sales` as S ON FS.`sale_tracking_Id` = S.sale_tracking_Id JOIN `Customers` as C ON FS.`UserId` = C.`id` WHERE FS.`DateofTransaction`= ? AND S.sale_type!='marketing' GROUP BY FS.`sale_tracking_Id`;",
+      "SELECT FS.id, FS.sale_tracking_Id, C.Name, FS.TotalAmount, FS.isSettled " +
+  "FROM `final_sale` as FS " +
+  "JOIN `sales` as S ON FS.`sale_tracking_Id` = S.sale_tracking_Id " +
+  "JOIN `Customers` as C ON FS.`UserId` = C.`id` " +
+  "WHERE FS.`DateofTransaction`= ? AND S.sale_type!='marketing' " +
+  "GROUP BY FS.sale_tracking_Id, FS.id, C.Name, FS.TotalAmount, FS.isSettled",
       [date]
     );
     res.json(sales);
