8b72bc2a2952d871d7dcb495c7b011acb6372c68
PrasanthMohan <prasanthmohan24@gmail.com>
2026-02-05 13:28:38 +0530

    opening and closing stock update

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -14,4 +14,18 @@ function getISTTimestamp(date = new Date()) {
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
-module.exports = { getISTTimestamp };
+function getISTDate(date = new Date()) {
+  const istOffset = 5.5 * 60 * 60 * 1000;
+  const istTime = new Date(date.getTime() + istOffset);
+
+  const year = istTime.getUTCFullYear();
+  const month = String(istTime.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(istTime.getUTCDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
+module.exports = {
+  getISTTimestamp,
+  getISTDate
+};
